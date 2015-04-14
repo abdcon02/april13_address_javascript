66d9af179161725b0c9a3eea623d3e826eb405d5
tanner heffner & connor abdelnoor <student@epicodus.com>
2015-04-14 09:46:28 -0700

    Add address object and method to join address properties.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,23 @@ $(function() {
     $('form#new-contact').submit(function(event) {
             var new_First_Name = $('input#new-first-name').val();
             var new_Last_Name = $('input#new-last-name').val();
-            var new_Address = $('input#new-address').val();
+            var new_Street = $('input#new-street').val();
+            var new_City = $('input#new-city').val();
+            var new_State = $('input#new-state').val();
+            var new_Zip = $('input#new-zip').val();
+            var new_Address = { street: new_Street,
+                                city: new_City,
+                                state: new_State,
+                                zip: new_Zip,
+                                fullAddress: function() {
+                                    return this.street + ", " + this.city + ", " + this.state + " " + this.zip;
+                                }
+                              };
 
             var newContact = { first_Name: new_First_Name,
                                last_Name: new_Last_Name,
-                               address: new_Address };
+                               address: new_Address.fullAddress()
+                             };
 
             $("ul#contacts").append("<li><span class='contact'>" +
                                 newContact.first_Name + " " +
@@ -14,7 +26,10 @@ $(function() {
 
             $("input#new-first-name").val("");
             $("input#new-last-name").val("");
-            $("input#new-address").val("");
+            $('input#new-street').val("");
+            $('input#new-city').val("");
+            $('input#new-state').val("");
+            $('input#new-zip').val("");
 
             $(".contact").last().click(function() {
                 $("#show-contact").show();
